8e718b3b0d841680295e2f154c84a0e5bcd11b9c
Peter Allin <peter@peca.dk>
2021-11-18 22:21:25 +0100

    Enforce specifying a host

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,29 @@
-pub struct Early {
-    scheme: Option<String>,
-    host: Option<String>,
-}
-
 pub struct EarlyNeedingScheme;
 
 impl EarlyNeedingScheme {
-    pub fn scheme<S: Into<String>>(self, scheme: S) -> Early {
-        Early {
-            scheme: Some(scheme.into()),
-            host: None,
+    pub fn scheme<S: Into<String>>(self, scheme: S) -> EarlyNeedingHost {
+        EarlyNeedingHost {
+            scheme: scheme.into(),
         }
     }
+}
+
+pub struct EarlyNeedingHost {
+    scheme: String,
+}
+
+impl EarlyNeedingHost {
+    pub fn host<S: Into<String>>(self, host: S) -> Early {
+        Early {
+            host: host.into(),
+            scheme: self.scheme,
+        }
+    }
+}
+
+pub struct Early {
+    scheme: String,
+    host: String,
 }
 
 impl Early {
@@ -20,33 +32,13 @@ impl Early {
     }
 
     pub fn build(self) -> String {
-        let scheme = self.scheme.map(|s| s + "://").unwrap_or("".into());
-        scheme + &self.host.unwrap_or_default()
-    }
-
-    pub fn host<S: Into<String>>(self, host: S) -> Self {
-        Early {
-            host: Some(host.into()),
-            ..self
-        }
+        self.scheme + "://" + &self.host
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Early;
-
-    #[test]
-    fn just_a_scheme_str() {
-        let url = Early::new().scheme("http").build();
-        assert_eq!("http://", url);
-    }
-
-    #[test]
-    fn just_a_scheme_string() {
-        let url = Early::new().scheme("http".to_string()).build();
-        assert_eq!("http://", url);
-    }
 
     #[test]
     fn scheme_plus_host_str() {
