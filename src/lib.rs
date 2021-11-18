3e994eaab0969fda6b8d159079fa8c3ac304f058
Peter Allin <peter@peca.dk>
2021-11-18 21:38:47 +0100

    A host name can now be added

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,31 @@
 struct Early {
     scheme: Option<String>,
+    host: Option<String>,
 }
 
 impl Early {
     fn new() -> Self {
-        Self { scheme: None }
+        Self {
+            scheme: None,
+            host: None,
+        }
     }
 
     fn build(self) -> String {
-        self.scheme.map(|s| s + "://").unwrap_or("".into())
+        let scheme = self.scheme.map(|s| s + "://").unwrap_or("".into());
+        scheme + &self.host.unwrap_or_default()
     }
 
     fn scheme(self, scheme: &str) -> Self {
         Early {
             scheme: Some(scheme.into()),
+            ..self
+        }
+    }
+
+    fn host(self, host: &str) -> Self {
+        Early {
+            host: Some(host.into()),
             ..self
         }
     }
@@ -33,5 +45,11 @@ mod tests {
     fn just_a_scheme() {
         let url = Early::new().scheme("http").build();
         assert_eq!("http://", url);
+    }
+
+    #[test]
+    fn scheme_plus_host() {
+        let url = Early::new().scheme("https").host("example.com").build();
+        assert_eq!("https://example.com", url);
     }
 }
