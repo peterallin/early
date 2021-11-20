40083b2edf389ed606a469a2bc16417750da72cd
Peter Allin <peter@peca.dk>
2021-11-20 18:19:34 +0100

    Simplify initial construction to need less handwaving
    Simply passing the 'scheme' and 'host' parameters to the 'new' function,
    this makes it eassier to describe the API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,5 @@
 use itertools::Itertools;
 
-pub struct EarlyNeedingScheme;
-
-impl EarlyNeedingScheme {
-    pub fn scheme<S: Into<String>>(self, scheme: S) -> EarlyNeedingHost {
-        EarlyNeedingHost {
-            scheme: scheme.into(),
-        }
-    }
-}
-
-pub struct EarlyNeedingHost {
-    scheme: String,
-}
-
-impl EarlyNeedingHost {
-    pub fn host<S: Into<String>>(self, host: S) -> Early {
-        Early {
-            host: host.into(),
-            scheme: self.scheme,
-            ..Default::default()
-        }
-    }
-}
-
 #[derive(Default)]
 pub struct Early {
     scheme: String,
@@ -34,9 +10,12 @@ pub struct Early {
 }
 
 impl Early {
-    #[allow(clippy::new_ret_no_self)] // Clippy is right, but I want it to look like there's only one type involved... might be a bad idea
-    pub fn new() -> EarlyNeedingScheme {
-        EarlyNeedingScheme
+    pub fn new<S1: Into<String>, S2: Into<String>>(scheme: S1, host: S2) -> Self {
+        Self {
+            scheme: scheme.into(),
+            host: host.into(),
+            ..Default::default()
+        }
     }
 
     pub fn port(self, port: u16) -> Self {
@@ -99,37 +78,25 @@ mod tests {
 
     #[test]
     fn scheme_plus_host_str() {
-        let url = Early::new()
-            .scheme("https")
-            .host("example.com")
-            .build();
+        let url = Early::new("https", "example.com").build();
         assert_eq!("https://example.com", url);
     }
 
     #[test]
     fn scheme_plus_host_string() {
-        let url = Early::new()
-            .scheme("https")
-            .host("example.com".to_string())
-            .build();
+        let url = Early::new("https", "example.com".to_string()).build();
         assert_eq!("https://example.com", url);
     }
 
     #[test]
     fn can_add_port() {
-        let url = Early::new()
-            .scheme("http")
-            .host("example.com")
-            .port(8080)
-            .build();
+        let url = Early::new("http", "example.com").port(8080).build();
         assert_eq!("http://example.com:8080", url);
     }
 
     #[test]
     fn can_add_single_query_key_value() {
-        let url = Early::new()
-            .scheme("https")
-            .host("example.com")
+        let url = Early::new("https", "example.com")
             .query("my_key", "my_value")
             .build();
         assert_eq!("https://example.com?my_key=my_value", url);
@@ -137,9 +104,7 @@ mod tests {
 
     #[test]
     fn can_add_two_query_key_values() {
-        let url = Early::new()
-            .scheme("https")
-            .host("example.com")
+        let url = Early::new("https", "example.com")
             .query("my_key1", "my_value1")
             .query("my_key2", "my_value2")
             .build();
@@ -151,18 +116,13 @@ mod tests {
 
     #[test]
     fn can_add_single_path_element() {
-        let url = Early::new()
-            .scheme("http")
-            .host("example.com")
-            .path("foo")
-            .build();
+        let url = Early::new("http", "example.com").path("foo").build();
         assert_eq!(url, "http://example.com/foo");
     }
+    
     #[test]
     fn can_add_multiple_path_elements() {
-        let url = Early::new()
-            .scheme("http")
-            .host("example.com")
+        let url = Early::new("http", "example.com")
             .path("foo")
             .path("bar")
             .path("baz")
